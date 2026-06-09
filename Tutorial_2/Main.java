d55466a50499b86a13ec888ddea7afa650275fb7
eltayebethar0-hub <eltayebethar0@gmail.com>
2026-06-09 04:38:35 +0000

    horrible code

---
+++ Tutorial_2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args) {
+
+        Student s1 = new Student ("Ethar", 22, 4);
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    }
+}
